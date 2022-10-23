83aadb0826a4c93304f6756a647fd8fe7aa1604b
Katherine Co <katherineco@cpp.edu>
2022-10-22 23:11:19 -0700

    initial restaurant-bill with comments

---
+++ restaurant/restaurant-bill.c
@@ -0,0 +1,22 @@
+// Katherine Co
+// CS2600
+// 10.19.22
+// Quiz #3: Restaurant Bill
+
+
+// Code for main function (must pass arguments)
+
+
+// Ask user for input ta and tip in percentage
+
+
+// Make sure user inputs valid format and valid numbers
+
+
+// Create a random number generator to use for the menu items
+
+
+// Use random number generator to choose menu itme and calculate tax and tip
+
+
+// Output tax and tip
