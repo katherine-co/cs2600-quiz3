7c085edfed188236e91134a140fb9bdb5ddb97eb
Katherine Co <katherineco@cpp.edu>
2022-10-22 21:09:42 -0700

    fixed PtrtoEmployee to PtrToEmployee

--- employee-search/employeeOne.c
+++ employee-search/employeeOne.c
@@ -1,8 +1,8 @@
 #include <string.h>
 #include "employee.h"
 
-PtrToEmployee searchEmployeeByNumber(PtrtoConstEmployee ptr, int tableSize, long targetNumber){
-    const PtrtoConstEmployee endPtr = ptr+tableSize;
+PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber){
+    const PtrToConstEmployee endPtr = ptr + tableSize;
     for(; ptr < endPtr; ptr++)
     {
         if(ptr->number == targetNumber) return (PtrToEmployee)ptr;
@@ -10,29 +10,29 @@ PtrToEmployee searchEmployeeByNumber(PtrtoConstEmployee ptr, int tableSize, long
     return NULL;
 }
 
-PtrToEmployee searchEmployeeByName(PtrtoConstEmployee ptr, int tableSize, char * targetName){
-    const PtrtoConstEmployee endPtr = ptr+tableSize;
-    for(; ptr<endPtr;ptr++)
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char * targetName){
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    for(; ptr < endPtr; ptr++)
     {
-        if(strcmp(ptr->name,targetName)==0) return (PtrToEmployee)ptr;
+        if(strcmp(ptr->name,targetName) == 0) return (PtrToEmployee)ptr;
     }
     return NULL;
 }
 
 // Code for searching employee by phone
-PtrToEmployee searchEmployeeByPhone(PtrtoConstEmployee ptr, int tableSize, char * targetPhone){
-    const PtrtoConstEmployee endPtr=ptr+tableSize;
-    for(; ptr<endPtr;ptr++)
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, char * targetPhone){
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    for(; ptr < endPtr; ptr++)
     {
-        if(strcmp(ptr->phone, targetPhone)==0) return (PtrToEmployee)ptr;
+        if(strcmp(ptr->phone, targetPhone) == 0) return (PtrToEmployee)ptr;
     }
     return NULL;
 }
 
 // Code for searching employee by salary
-PtrToEmployee searchEmployeeBySalary(PtrtoConstEmployee ptr, int tableSize, double targetSalary){
-    const PtrtoConstEmployee endPtr=ptr+tableSize;
-    for(; ptr<endPtr;ptr++)
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double targetSalary){
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    for(; ptr < endPtr; ptr++)
     {
         if(ptr->salary == targetSalary) return (PtrToEmployee)ptr;
     }
