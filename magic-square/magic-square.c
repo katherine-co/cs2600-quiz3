b9e10387d4280935e0300cc822c5d5f066b88288
Katherine Co <katherineco@cpp.edu>
2022-10-23 04:42:34 -0700

    initial magic-sqaure with comments

---
+++ magic-square/magic-square.c
@@ -0,0 +1,25 @@
+// Katherine Co
+// CS2600
+// 10.22.22
+// Quiz #3: Lo Shu Magic Square
+
+
+// Function that accepts two-dimensional array
+
+
+// Use random number generator
+
+
+// Generate Lo Shu Square
+
+
+// Loop through magic square to put random nubmers
+
+
+// After Lo Shu square is filled, check if square if a Lo SHu
+
+
+    // Swap numbers until you get a Lo Shu square
+
+
+// Add counter to count how many attempts before success
