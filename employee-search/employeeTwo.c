b2358cff51885dc247f132ebc891d582150737e6
Katherine Co <katherineco@cpp.edu>
2022-10-22 20:28:49 -0700

    fixed PtrtoEmployee to PtrToEmployee

--- employee-search/employeeTwo.c
+++ employee-search/employeeTwo.c
@@ -9,54 +9,54 @@
     functionPtr - points to a comparison function below and will perform the associated check
 */
 
-static PtrToEmployee searchEmployeeTable(PtrtoConstEmployee ptr, int tableSize, const void *targetPtr,
-                                         int (*functionPtr)(const void *, PtrtoConstEmployee))
+static PtrToEmployee searchEmployeeTable(PtrToConstEmployee ptr, int tableSize, const void *targetPtr,
+                                         int (*functionPtr)(const void *, PtrToConstEmployee))
 {
-    PtrtoConstEmployee endPtr = ptr+tableSize;
+    PtrToConstEmployee endPtr = ptr+tableSize;
     for(; ptr<endPtr; ptr++)
-        if((*functionPtr)(targetPtr, ptr)==0)
+        if((*functionPtr)(targetPtr, ptr) == 0)
             return (PtrToEmployee) ptr;
     return NULL;
 }                                                                       
 
 //The functionPtr will point to one of these comparison functions to perform a check 
-static int compareEmployeeNumber(const void *targetPtr, PtrtoConstEmployee tableValuePtr)
+static int compareEmployeeNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr)
 {
     return *(long*) targetPtr != tableValuePtr->number; //const void *targetPtr ==> typecast as int pointer then dereference
 }
 
-static int compareEmployeeName(const void *targetPtr, PtrtoConstEmployee tableValuePtr)
+static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableValuePtr)
 {
     return strcmp((char*)targetPtr, tableValuePtr->name);   //const void *targetPtr ==> typecast as char pointer then pass into strcmp()
 }
 
-static int compareEmployeePhone(const void *targetPtr, PtrtoConstEmployee tableValuePtr)
+static int compareEmployeePhone(const void *targetPtr, PtrToConstEmployee tableValuePtr)
 {
     return strcmp((char*) targetPtr, tableValuePtr->phone);
 }
 
-static int compareEmployeeSalary(const void *targetPtr, PtrtoConstEmployee tableValuePtr)
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr)
 {
     return *(double*) targetPtr != tableValuePtr->salary;
 }
 
 //These are called wrappers. These functions are what you will use in your main!!!
-PtrToEmployee searchEmployeeByNumber(PtrtoConstEmployee ptr, int size, long number)
+PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number)
 {
     return searchEmployeeTable(ptr, size, &number, compareEmployeeNumber);
 }
 
-PtrToEmployee searchEmployeeByName(PtrtoConstEmployee ptr, int size, char *name)
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char* name)
 {
-    return searchEmployeeTable(ptr,size,&name,compareEmployeeName);
+    return searchEmployeeTable(ptr,size, name,compareEmployeeName);
 }
 
-PtrToEmployee searchEmployeeByPhone(PtrtoConstEmployee ptr, int size, char *phone)
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char* phone)
 {
-    return searchEmployeeTable(ptr, size, &phone, compareEmployeePhone);
+    return searchEmployeeTable(ptr, size, phone, compareEmployeePhone);
 }
 
-PtrToEmployee searchEmployeeBySalary(PtrtoConstEmployee ptr, int size, double salary)
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary)
 {
     return searchEmployeeTable(ptr, size,&salary, compareEmployeeSalary);
 }
