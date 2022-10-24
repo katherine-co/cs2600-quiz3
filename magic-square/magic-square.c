43f85807494111fef385f1bdccab362d74f68436
Katherine Co <katherineco@cpp.edu>
2022-10-23 18:43:08 -0700

    redid code bc first run was indefinite

--- magic-square/magic-square.c
+++ magic-square/magic-square.c
@@ -2,102 +2,109 @@
 // CS2600
 // 10.22.22
 // Quiz #3: Lo Shu Magic Square
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
  
+int magicSqur[3][3] =   //Empty Lo Shu Square
+{
+    {0,0,0},
+    {0,0,0},
+    {0,0,0}
+};
+
+int populateArray[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
 // Swap numbers until you get a Lo Shu square
-void swapVal(int *a, int *b)
+void swapVal()
 {
-    int c = *a;
-    *b = c;
-    *a = *b;
-}
- 
-// Use random number generator to generate array
-void randArrayGen(int array[], int a)
-{
-    time_t t;
-    srand((unsigned) time(&t));
- 
-    for (int x = a - 1; x > 0; x--)
+    for(int i = 8; i > 0; i--)
     {
-        int y = rand() % (x + 1);
-        swapVal(&array[x], &array[y]);
+      int j = (rand() % (i + 1));
+      int temp = populateArray[i];
+
+      populateArray[i] = populateArray[j];
+      populateArray[j] = temp;
     }
 }
- 
-// Generate Lo Shu Square
-int main()
+
+int foundMagic(int magicSqur[3][3])
 {
-    printf("Welcome to Lo Shu Magic Square\n");
- 
-    bool rows = false;
-    bool columns = false;
-    bool diagonals = false;
-    int count = 0;
-    int magicSqur[3][3];
-    int index = 0;
-    int populateArray[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-   
-    if (rows == false || columns == false || diagonals == false)
+    int magic = 1;
+
+    // After Lo Shu square is filled, check if square is a Lo Shu
+    int diagonal1 = magicSqur[0][0] + magicSqur[1][1] + magicSqur[2][2];    // Checks diagonals
+    if(diagonal1 != 15) magic = 0;
+    int diagonal2 = magicSqur[0][2] + magicSqur[1][1] + magicSqur[2][0];
+    if(diagonal2 != 15) magic = 0;
+
+    int row1 = magicSqur[0][0] + magicSqur[0][1] + magicSqur[0][2];     // Checks rows
+    if(row1 != 15) magic = 0;
+    int row2 = magicSqur[1][0] + magicSqur[1][1] + magicSqur[1][2];
+    if(row2 != 15) magic = 0;
+    int row3 = magicSqur[2][0] + magicSqur[2][1] + magicSqur[2][2];
+    if(row3 != 15) magic = 0;
+
+    int column1 = magicSqur[0][0] + magicSqur[1][0] + magicSqur[2][0];  // Checks columns
+    if(column1 != 15) magic = 0;
+    int column2 = magicSqur[0][1] + magicSqur[1][1] + magicSqur[2][1];
+    if(column2 != 15) magic = 0;
+    int column3 = magicSqur[0][2] + magicSqur[1][2] + magicSqur[2][2];
+    if(column3 != 15) magic = 0;
+
+    return magic;
+}
+
+void printSqur(int arr[3][3])   // Printing the Lo Shu square
+{
+    int i;
+    int j;
+    for (i = 0; i < 3; i++)
     {
-        do
+        for (j = 0; j < 3; j++)
         {
-            rows = false;
-            columns = false;
-            diagonals = false;
-       
-            // Loop through magic square to put random nubmers
-            int s = sizeof(populateArray) / sizeof(populateArray[0]);
-            randArrayGen(populateArray, s);
-           
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    magicSqur[i][j] = populateArray[index];
-                    index++;
-                }
-            }
- 
-            index = 0;
-            int sum = 15;
- 
-            // After Lo Shu square is filled, check if square is a Lo Shu
-            int row1 = magicSqur[0][0] + magicSqur[0][1] + magicSqur[0][2];
-            int row2 = magicSqur[1][0] + magicSqur[1][1] + magicSqur[1][2];
-            int row3 = magicSqur[2][0] + magicSqur[2][1] + magicSqur[2][2];
-            if (row1 == 15 && row2 == 15 && row3 == 15) rows = true;
- 
-            int column1 = magicSqur[0][0] + magicSqur[1][0] + magicSqur[2][0];
-            int column2 = magicSqur[0][1] + magicSqur[1][1] + magicSqur[2][1];
-            int column3 = magicSqur[0][2] + magicSqur[1][2] + magicSqur[2][2];
-            if (column1 == 15 && column2 == 15 && column3 == 15) columns = true;
- 
-            int diagonal1 = magicSqur[0][0] + magicSqur[1][1] + magicSqur[2][2];
-            int diagonal2 = magicSqur[0][2] + magicSqur[1][1] + magicSqur[2][0];
-            if (diagonal1 == 15 && diagonal2 == 15) diagonals = true;
- 
-            // Add counter to count how many attempts before success
-            count++;
-        }
-        while (rows == false || columns == false || diagonals == false);
-    }
- 
-    printf("Printing a Lo Shu Magic Square:\n");
-    for (int i = 0; i < 3; i++)
-    {
-        printf("[ ");
-        for (int j = 0; j < 3; j++)
-        {
-            printf("%d ", magicSqur[i][j]);
+            if(j == 0) printf("[ ");
+            printf("%d ", arr[i][j]);
         }
         printf("]\n");
     }
+}
+
+// Generate Lo Shu Square
+void createGrid()
+{
+    swapVal();
+    int n = 0;
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            magicSqur[i][j] = populateArray[n];
+            n++;
+        }
+    }
+
+}
  
-    printf("It took %d attempts to get a Lo Shu Magic Square", count);
+int main()
+{  
+    printf("Welcome to Lo Shu Magic Square!\n");
+    srand(time(0));
  
+    int found = 0;
+    int count = 0;
+    while(!found) {     // Continues generating grid until Lo Shu is found
+        createGrid();
+        count++;
+ 
+        if(foundMagic(magicSqur) == 1) found = 1;
+    }
+    printf("Printing a Lo Shu Magic Square:\n");
+    printSqur(magicSqur);
+    printf("It took %d attempts to get a Lo Shu Magic Square.", count);
+
     return EXIT_SUCCESS;
 }
