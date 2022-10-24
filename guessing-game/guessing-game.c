b6331631e722da98efacbaeb2fb98ceae5229c09
Katherine Co <katherineco@cpp.edu>
2022-10-23 19:00:37 -0700

    initial guessing-game with comments

---
+++ guessing-game/guessing-game.c
@@ -0,0 +1,24 @@
+// Katherine Co
+// CS2600
+// 10.23.22
+// Quiz #3: Number Guessing Game
+
+// Display menu for user to choose option between 1-3
+
+
+// If option 1 selected, ask user to enter number
+
+    // If user guess correct number, have program go back to menu
+
+    // Else tell them if they were too low or high
+
+        // Allow them to guess again until they get correct aswer
+
+        // Check if user input puts 'q' to exit back to menu
+
+// if option 2 selected, tell max value they can set the number
+
+    // Check number is <= 0 or > 10
+
+// If option 3 selected, display message "Thank you" and end program
+
